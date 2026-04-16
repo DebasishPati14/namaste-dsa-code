ecbce0206d006460c74ec92d5f9bfc3a16bfd3fb
Mr.Debasish <debasishpati14@gmail.com>
2026-04-16 21:59:12 +0530

    yester day worked on this

--- src/s03-recursion/chatgpt-solutions.ts
+++ src/s03-recursion/chatgpt-solutions.ts
@@ -1,7 +1,7 @@
 // ===============================
 // Question 1: Sum of Digits
 
-import ts = require("typescript");
+import ts = require('typescript');
 
 // ===============================
 let totalSumOfDigits = 0;
@@ -19,9 +19,6 @@ function findSumOfDigits(num: number) {
 
 const n1 = 1234;
 // console.log(`Total sum of digits in ${n1} is: ${findSumOfDigits(n1)}`);
-
-
-
 
 // ===============================
 // Question 2: Reverse a Number
@@ -42,94 +39,101 @@ function findReverseNum(num: number) {
 const n2 = 1234;
 //console.log(`The reverse of number ${n2} is: ${findReverseNum(n2)}`);
 
-
-
-
 // ===============================
 // Question 3: Check Palindrome Number and String
 // ===============================
-function isStringPalindrome(orgString: string, revString: string = '', curIdx: number = orgString.length - 1){
-  console.log({ revString})
-  if(curIdx < 0){
+function isStringPalindrome(
+  orgString: string,
+  revString: string = '',
+  curIdx: number = orgString.length - 1,
+) {
+  console.log({ revString });
+  if (curIdx < 0) {
     return orgString === revString;
-  }else{
-    revString += orgString.charAt(curIdx)
+  } else {
+    revString += orgString.charAt(curIdx);
     curIdx--;
-    return isStringPalindrome(orgString, revString, curIdx)
+    return isStringPalindrome(orgString, revString, curIdx);
   }
 }
 
-function isNumberPalindrome(num: number){
+function isNumberPalindrome(num: number) {}
 
-}
-
-const s3 = "recursionoisrucer";
+const s3 = 'recursionoisrucer';
 const n3 = 121;
 
 //console.log(`Is number ${n3} a palindrome? ${isNumberPalindrome(n3)}`);
 //console.log(`Is string ${s3} a palindrome? ${isStringPalindrome(s3)}`);
 
-
 // ===============================
 // QUESTION 4: Remove all occurance of a char
 // ===============================
-function removeOccuranceChar(orgStr: string, remChar: string, newStr:string = '', curIdx: number = 0){
-    if(curIdx == orgStr.length){
-        return newStr;
+function removeOccuranceChar(
+  orgStr: string,
+  remChar: string,
+  newStr: string = '',
+  curIdx: number = 0,
+) {
+  if (curIdx == orgStr.length) {
+    return newStr;
+  } else {
+    if (orgStr.charAt(curIdx) != remChar) {
+      newStr += orgStr.charAt(curIdx);
     }
-    else {
-        if(orgStr.charAt(curIdx) != remChar){
-            newStr += orgStr.charAt(curIdx); 
-        }
-        curIdx++;
-        return removeOccuranceChar(orgStr, remChar, newStr, curIdx);
-    }
+    curIdx++;
+    return removeOccuranceChar(orgStr, remChar, newStr, curIdx);
+  }
 }
 
 const str4 = 'recursion';
 const targetChar = 'r';
 //console.log(`The ${str4} will be after removing the char ${targetChar} is: ${ removeOccuranceChar(str4, targetChar, '', 0) }`);
 
-
-
-
 // ===============================
 // Question 5: Is Araay Sorted
 // ===============================
 
-function isArraySorted(nums: number[], curIdx: number = 0){
-    if(nums.length == curIdx){
-        return true;
-    }else{
-        if(nums[curIdx]! > nums[curIdx + 1]!){
-            return false;
-        }
-        curIdx++;
-       return isArraySorted(nums, curIdx);
+function isArraySorted(nums: number[], curIdx: number = 0) {
+  if (nums.length == curIdx) {
+    return true;
+  } else {
+    if (nums[curIdx]! > nums[curIdx + 1]!) {
+      return false;
     }
+    curIdx++;
+    return isArraySorted(nums, curIdx);
+  }
 }
 
 const nums5 = [1, 2, 3, 4, 5];
-console.log(`Check if the array of ${nums5} is sorted: ${ isArraySorted(nums5)}`);
-
-
-
-
+console.log(
+  `Check if the array of ${nums5} is sorted: ${isArraySorted(nums5)}`,
+);
 
 // ===============================
 // Question 6: All Indices Of Array
 // ===============================
 
-function findIndices(num: number){
-
+function findIndices(
+  nums: number[],
+  target: number,
+  resArr: number[] = [],
+  curIdx: number = 0,
+): number[] {
+  if (curIdx == nums.length) {
+    return resArr;
+  } else {
+    if (nums[curIdx] != target) {
+      resArr.push(nums[curIdx]!);
+    }
+    curIdx++;
+    return findIndices(nums, target, resArr, curIdx);
+  }
 }
 
-const n6 = 7;
-console.log(`Is ${n6} a prime number? ${findIndices(n6)}`);
-
-
-
-
+const num6 = [1, 2, 3, 2, 4, 2];
+const n6 = 2;
+console.log(`Is ${num6} a prime number? [${findIndices(num6, n6)}]`);
 
 // ===============================
 // Question 7: Fibonacci (nth term)
@@ -143,9 +147,6 @@ const n7 = 6;
 console.log(`Fibonacci term at position ${n7}: ${fibonacci(n7)}`);
 */
 
-
-
-
 // ===============================
 // Question 8: Find Largest Digit
 // ===============================
@@ -158,10 +159,6 @@ const n8 = 93847;
 console.log(`Largest digit in ${n8}: ${findLargestDigit(n8)}`);
 */
 
-
-
-
-
 // ===============================
 // Question 9: Sum of First N Natural Numbers
 // ===============================
@@ -173,9 +170,6 @@ function sumOfNaturalNumbers(num: number){
 const n9 = 10;
 console.log(`Sum of first ${n9} natural numbers: ${sumOfNaturalNumbers(n9)}`);
 */
-
-
-
 
 // ===============================
 // Question 10: Check Armstrong Number
