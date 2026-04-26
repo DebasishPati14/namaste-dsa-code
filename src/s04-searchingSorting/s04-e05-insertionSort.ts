cb88ccf6c4359de51fcde9a695085cd9f0a6fac6
Mr.Debasish <debasishpati14@gmail.com>
2026-04-26 23:18:01 +0530

    merge sort is also required in spaced repeatation

--- src/s04-searchingSorting/s04-e05-insertionSort.ts
+++ src/s04-searchingSorting/s04-e05-insertionSort.ts
@@ -15,10 +15,27 @@ function insertionSort(nums: number[]) {
       curIdx--;
     }
     sortedArray[curIdx + 1] = curEle;
-    console.log({sortedArray, curIdx, curEle, i})
-      }
+    console.log({ sortedArray, curIdx, curEle, i });
+  }
   return sortedArray;
 }
 
+function insertionSortOriginal(nums: number[]) {
+  for (let i = 1; i < nums.length; i++) {
+    const element = nums[i]!;
+    let prevIdx = i - 1;
+
+    while (prevIdx >= 0 && nums[prevIdx]! > element) {
+      nums[prevIdx + 1]! = nums[prevIdx]!;
+      prevIdx--;
+    }
+    console.log({ element, prevIdx });
+    nums[prevIdx + 1]! = element;
+  }
+  return nums;
+}
+
 const nums3 = [5, 6, 4, 2, 1, 3];
-console.log(`The sorting of the array ${nums3} is: ${insertionSort(nums3)}`);
+console.log(
+  `The sorting of the array ${nums3} is: \n [${insertionSortOriginal(nums3)}]\n [${insertionSort(nums3)}]`,
+);
