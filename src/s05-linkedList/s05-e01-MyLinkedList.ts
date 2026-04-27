752acd5209eafc98f9b3d58068759a2cd28a6c93
DebasishPati14 <debasishpati14@gmail.com>
2026-04-28 00:04:07 +0530

    forgot to add

--- src/s05-linkedList/s05-e01-MyLinkedList.ts
+++ src/s05-linkedList/s05-e01-MyLinkedList.ts
@@ -46,16 +46,53 @@ class MyLinkedList {
   }
 
   addAtIndex(index: number, val: number) {
+    if (index > this.size - 1) {
+      return 0;
+    }
+
+    let curNode: typeof MyNode | null = this.head;
+
+    for (let i = 0; i < this.size; i++) {
+      if (i + 1 == index) {
+        const myNode = new MyNode(val);
+        myNode.next = curNode.next;
+        curNode.next = myNode;
+      }
+      curNode = curNode.next;
+    }
+
     this.size = this.size + 1;
   }
 
   deleteAtIndex(index: number) {
-    this.size = this.size - 1;
+    if (index > this.size - 1) {
+      return 0;
+    }
+
+    let curNode: typeof MyNode | null = this.head;
+
+    for (let i = 0; i < this.size; i++) {
+      if (i + 1 == index) {
+        curNode.next = curNode.next.next;
+      }
+      curNode = curNode.next;
+    }
+    return 0;
   }
 }
 const myLinkedList = new MyLinkedList();
-myLinkedList.addAtHead(5);
+myLinkedList.addAtHead(1);
 console.log({ head: myLinkedList.head, siz: myLinkedList.size });
+myLinkedList.addAtTail(5);
 myLinkedList.addAtTail(6);
+myLinkedList.addAtTail(7);
 console.log({ tail: myLinkedList.tail, siz: myLinkedList.size });
-console.log({ at2: myLinkedList.get(1) });
+myLinkedList.addAtIndex(1, 2);
+myLinkedList.addAtIndex(2, 3);
+myLinkedList.addAtIndex(3, 4);
+console.log({
+  at2: myLinkedList.get(1),
+  at3: myLinkedList.get(2),
+  at4: myLinkedList.get(3),
+  siz: myLinkedList.size,
+});
