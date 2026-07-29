9e3901a77505016c7150835a2e55db73a46b0939
Mr.Debasish <debasishpati14@gmail.com>
2026-07-29 23:34:25 +0530

    Had to review the notebook

--- src/repeated-iteration/index.ts
+++ src/repeated-iteration/index.ts
@@ -61,7 +61,7 @@ function buySellStock(priceArr: number[]) {
 
 const prices = [7, 6, 4, 3, 1];
 buySellStock(prices);
-*/
+
 
 // Question - 3
 //Find the pivot index (LC-724)
@@ -86,3 +86,70 @@ function findPivotIdx(numArr: number[]) {
 
 const nums = [1, 2, 3];
 findPivotIdx(nums);
+
+
+// Question - 4
+// Majority Element (LC-169) -BOOYRE"S MOORE SOL
+function findMajorityElement(numsArr: number[]) {
+  let candidate = numsArr[0];
+  let count = 1;
+  for (let i = 0; i < numsArr.length; i++) {
+    if (numsArr[i] == candidate) {
+      count++;
+    } else {
+      if (count == 0) {
+        candidate = numsArr[i];
+        count = 1;
+      } else {
+        count--;
+      }
+    }
+  }
+  console.log({ candidate });
+  return candidate;
+}
+
+const numbers = [2, 2, 1, 1, 1, 1, 1, 2, 2];
+findMajorityElement(numbers);
+*/
+
+// Question - 5
+// Rotate an array to it's right(LC-189)
+function rotateArray(numsArr: number[], k: number) {
+  // 1st rotate the array
+  for (
+    let i = 0, j = numberArr.length - 1;
+    i <= numsArr.length / 2, j >= numsArr.length / 2;
+    i++, j--
+  ) {
+    const temp = numberArr[i];
+    numberArr[i] = numberArr[j];
+    numberArr[j] = temp;
+  }
+  console.log({ numberArr });
+
+  // 2nd rotate from 0 to K
+  for (let i = 0, j = k - 1; i <= k / 2, j >= k / 2; i++, j--) {
+    const temp = numberArr[i];
+    numberArr[i] = numberArr[j];
+    numberArr[j] = temp;
+  }
+  console.log({ numberArr });
+
+  // 3rd rotate from K to end
+  for (
+    let i = k, j = numberArr.length - 1;
+    i <= (numsArr.length + k) / 2, j >= (numsArr.length + k) / 2;
+    i++, j--
+  ) {
+    console.log({ Before: numberArr, i, j });
+    const temp = numberArr[i];
+    numberArr[i] = numberArr[j];
+    numberArr[j] = temp;
+    console.log({ After: numberArr, i, j });
+  }
+}
+
+const numberArr = [1, 2, 3, 4, 5, 6, 7],
+  idx = 3;
+rotateArray(numberArr, idx);
